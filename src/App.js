73447f534291e3772795d87c0deee2411820b317
Thitichaya <thitichayamaklai39@gmail.com>
2018-07-01 10:12:23 +0700

    commit message

--- src/App.js
+++ src/App.js
@@ -3,58 +3,60 @@ import FormSubmit from "./components/FormSubmit";
 import List from "./components/List";
 import HeaderComponents from "./components/HeaderComponents";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { id: 1, name: "pooklook", complete: false },
+        { id: 2, name: "nuheng", complete: false }
+      ],
+      message: ""
+    };
 
-
-  class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-       todos: [
-         {id:1, name: "pooklook", complete:false},
-         {id:2, name: "nuheng", complete:false}
-       ],
-       message: ""
-     };
-    
-     this.onChangeMessage = this.onChangeMessage.bind(this);
-     this.onSubmitMessage = this.onSubmitMessage.bind(this);
-    }
-    
-    onChangeMessage(e){
-      this.setState({message: e.target.value });
-    }
-    onSubmitMessage(e){
-      e.preventDefault();
-      let oldTodo = this.state.todos;
-      let todoLength = this.state.todos.length;
-      let LastId = this.state.todos[todoLength-1].id
-      let NewMessage ={
-        id: LastId +1, name: this.state.message, complete: false};      
-      oldTodo.push(NewMessage);
-      this.setState({todo: oldTodo});
-    }
-
-    render() {
-      return (
-        <div
-          style={{
-            borderColor: "#e12c6a",
-            borderWidth: 2,
-            borderStyle: "solid",
-            borderRadius: 4,
-            width: 1024,
-            margin: "auto",
-            marginTop: 20
-          }}
-        >
-          <HeaderComponents />
-          <List todos={this.state.todos}/>
-              
-          
-          <FormSubmit onChangeMessage = {this.onChangeMessage}
-          onSubmitMessage = {this.onSubmitMessage} />
-          </div>
-      );
-    }
+    this.onChangeMessage = this.onChangeMessage.bind(this);
+    this.onSubmitMessage = this.onSubmitMessage.bind(this);
   }
-  export default App;
+
+  onChangeMessage(e) {
+    this.setState({ message: e.target.value });
+  }
+  onSubmitMessage(e) {
+    e.preventDefault();
+    let oldTodo = this.state.todos;
+    let todoLength = this.state.todos.length;
+    let LastId = this.state.todos[todoLength - 1].id;
+    let NewMessage = {
+      id: LastId + 1,
+      name: this.state.message,
+      complete: false
+    };
+    oldTodo.push(NewMessage);
+    this.setState({ todo: oldTodo });
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          borderColor: "#e12c6a",
+          borderWidth: 2,
+          borderStyle: "solid",
+          borderRadius: 4,
+          width: 1024,
+          margin: "auto",
+          marginTop: 20
+        }}
+      >
+        <HeaderComponents />
+        <List todos={this.state.todos} />
+
+        <FormSubmit
+          onChangeMessage={this.onChangeMessage}
+          onSubmitMessage={this.onSubmitMessage}
+        />
+      </div>
+    );
+  }
+}
+export default App;
